e368a82cc42ff9c2098b2b3eb263bf29487f3438
41vi4p <dav7.digi@yahoo.com>
2025-07-23 21:39:54 +0530

    cap_init

---
+++ android/app/src/main/java/com/medistock/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.medistock.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.medistock.app',
+  appName: 'MediStock',
+  webDir: 'out'
+};
+
+export default config;
